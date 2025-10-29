fe122a83d0b8b6596a0c377f6d7361471d921c58
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-10-29 18:16:14 +0300

    add applicatio themes

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primarySwatch: Colors.indigo,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      backgroundColor: Colors.indigo,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      ),
+    ),
+  );
+}
